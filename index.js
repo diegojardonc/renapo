e70ca95f0c182aca93735948a3e9d85a40f90ec1
Diego Jardon <diegojardonc@gmail.com>
2018-07-03 15:46:25 -0500

    Fixed date showing a day before due to 0 based value On submit, form no longer redirects

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-const clearCURP = document.getElementById("clearCURPForm");
-const clearINFO = document.getElementById("clearINFOForm");
-const CURPInputs = document.querySelectorAll(".get-curp input[type='text']");
-const INFOInputs = document.querySelectorAll(".get-info input[type='text']");
+const clearCURP = document.getElementById('clearCURPForm');
+const clearINFO = document.getElementById('clearINFOForm');
+const CURPInputs = Array.from(document.querySelectorAll('.get-curp input[type="text"]'));
+const INFOInputs = Array.from(document.querySelectorAll('.get-info input[type="text"]'));
+const forms = document.querySelectorAll('form');
 const date = new Date();
 
-let months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Noviembre', 'Diciembre'];
+let months = [
+  'Enero',
+  'Febrero',
+  'Marzo',
+  'Abril',
+  'Mayo',
+  'Junio',
+  'Julio',
+  'Agosto',
+  'Septiembre',
+  'Noviembre',
+  'Diciembre'
+];
 
-let day = date.getDay();
+let day = date.getDay() + 1;
 let month = months[date.getMonth()];
 let year = date.getFullYear();
+let today = day + ' de ' + month + ' del ' + year;
 
-let today = `${day} de ${month} del ${year}`;
+console.log({today})
 
-clearCURP.addEventListener("click", () => {
-  for(i = 0; i < CURPInputs.length; i++){
-    CURPInputs.item(i).setProperty("value", "");
-  }
+clearCURP.addEventListener('click', function (e) {
+  e.preventDefault();
+  CURPInputs.forEach(input => input.value = '');
 });
 
-clearINFO.addEventListener("click", () => {});
+clearINFO.addEventListener('click', function (e) {
+  e.preventDefault();
+  INFOInputs.forEach(function (input) {
+    input.value = '';
+  });
+});
 
-document.getElementById("date").innerHTML = today;
+forms.forEach(function (form) {
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+  });
+});
+
+document.getElementById('date').innerHTML = today;
